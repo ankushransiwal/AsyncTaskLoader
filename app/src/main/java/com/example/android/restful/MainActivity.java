64c995196cc105cd96cf1658fda83886e4eb3d79
Ankush <ankushransiwal.13@gmail.com>
2018-07-13 19:38:47 +0530

    Add AsyncTask Loader to handle change config

--- app/src/main/java/com/example/android/restful/MainActivity.java
+++ app/src/main/java/com/example/android/restful/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.restful;
 
+import android.content.AsyncTaskLoader;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
     TextView output;
 
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void runClickHandler(View view) {
+        //This will initialise the loader manager class and move the flow to callback function onCreateloader
+        getSupportLoaderManager().initLoader(0,null,this).forceLoad();
 
     }
 
@@ -25,4 +31,40 @@ public class MainActivity extends AppCompatActivity {
         output.setText("");
     }
 
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        output.append("Creating the Loader\n");
+        //This creates an instance of MytaskLoader and performs the  task
+        return new MyTaskLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        output.append("Loader finished, returned : " + data +"\n");
+
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
+    }
+
+    //Make sure to extend one that is in your support library com.android.supoort.v4.. or else erorrrrsss
+    private static class MyTaskLoader extends android.support.v4.content.AsyncTaskLoader<String>{
+
+        public MyTaskLoader(Context context) {
+            super(context);
+        }
+
+        @Override
+        public String loadInBackground() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "From the loader";
+        }
+    }
 }
